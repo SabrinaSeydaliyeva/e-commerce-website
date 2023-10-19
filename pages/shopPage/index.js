b808f423714996cd7aada43a60f79d90ed357ae5
SabrinaSeydaliyeva <seydaliyeva.sabrina@bk.ru>
2023-10-19 16:09:05 +0400

    ss

--- pages/shopPage/index.js
+++ pages/shopPage/index.js
@@ -1,15 +1,12 @@
-let main=document.querySelector("main ul")
+let main = document.querySelector("main ul");
 
+function getProduct() {
+  let product = JSON.parse(localStorage.getItem("products"));
 
+  console.log(product);
 
-function getProduct(){
-    let product=JSON.parse(localStorage.getItem("products"))
-
-    console.log(product);
-
-    for (let index = 0; index < product.length; index++) {
-        main.innerHTML+=
-        `
+  for (let index = 0; index < product.length; index++) {
+    main.innerHTML += `
         <li>
             <img src="${product[index].img}" alt="" />
 
@@ -39,18 +36,7 @@ function getProduct(){
               </div>
 
               <div class="heart">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M12 21.35L10.55 20.03C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3C9.24 3 10.91 3.81 12 5.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5C22 12.27 18.6 15.36 13.45 20.03L12 21.35Z"
-                    fill="#DB6B97"
-                  />
-                </svg>
+              <img src="./images/no-fav.png" alt="">
               </div>
             </div>
 
@@ -61,10 +47,21 @@ function getProduct(){
             <span>${product[index].amount} azn</span>
             <button>Add to cart</button>
           </li>
-        `
-        
-        
-    }
+        `;
+  }
+  let noFavImg = document.querySelectorAll(".heart img");
+
+  noFavImg.forEach((element) => {
+    element.addEventListener("click", () => changeImg(element));
+  });
+
+  console.log(noFavImg);
 }
 
-getProduct()
+let heart = false;
+
+function changeImg(element) {
+  element.src = "./images/fav.png ";
+}
+
+getProduct();
